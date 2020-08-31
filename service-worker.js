0f3e68a71e159df9cd8cbdd5768e08407d64ba62
RusTron <rusan.tron@gmail.com>
2020-08-31 15:51:34 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.2396f3cea785e9c0f063c7f89c80e971.js"
 );
 
 self.addEventListener('message', (event) => {
